b89e271c58291167c50778aa9ab8a2aa0a61ce58
thu0cl4o <vuhoang5794@gmail.com>
2022-06-19 21:48:15 +0700

    update loop forever with push data to kafka

--- src/main/java/com/viettel/vtcc/master/data/report/bigdata/process/ProcessDataPublisher.java
+++ src/main/java/com/viettel/vtcc/master/data/report/bigdata/process/ProcessDataPublisher.java
@@ -18,34 +18,38 @@ public class ProcessDataPublisher {
 
     public static void main(String[] args) {
         // push forum msg to kafka topic forum_data
+
         Thread thread = new Thread(() -> {
-            for (File file : new File(FOLDER_FORUM).listFiles()) {
-                try {
+            while (true){
+                for (File file : new File(FOLDER_FORUM).listFiles()) {
                     try {
-                        List<String> listLines = FileUtils.readLines(file, "UTF-8");
-                        listLines.forEach(line -> kafkaPublisher.push_data(line, "forum_data"));
+                        try {
+                            List<String> listLines = FileUtils.readLines(file, "UTF-8");
+                            listLines.forEach(line -> kafkaPublisher.push_data(line, "forum_data"));
+                        } catch (Exception e) {
+                            log.error(e.getMessage(), e);
+                        }
                     } catch (Exception e) {
                         log.error(e.getMessage(), e);
                     }
-                } catch (Exception e) {
-                    log.error(e.getMessage(), e);
                 }
             }
-
         });
         thread.start();
         // push telegram msg to kafka topic telegram_data
         new Thread(() -> {
-            for (File file : new File(FOLDER_TELEGRAM).listFiles()) {
-                try {
+            while (true){
+                for (File file : new File(FOLDER_TELEGRAM).listFiles()) {
                     try {
-                        List<String> listLines = FileUtils.readLines(file, "UTF-8");
-                        listLines.forEach(line -> kafkaPublisher.push_data(line, "telegram_data"));
+                        try {
+                            List<String> listLines = FileUtils.readLines(file, "UTF-8");
+                            listLines.forEach(line -> kafkaPublisher.push_data(line, "telegram_data"));
+                        } catch (Exception e) {
+                            log.error(e.getMessage(), e);
+                        }
                     } catch (Exception e) {
                         log.error(e.getMessage(), e);
                     }
-                } catch (Exception e) {
-                    log.error(e.getMessage(), e);
                 }
             }
         }).start();
